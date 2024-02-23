6ca84874576aeeef215b381bc2c26aa8f68e3cca
claudinei <claudineigaiteiro@gmail.com>
2024-02-22 21:48:12 -0300

    Ajuste index principal

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const port = 3000;
 
 app.get('/', (req, res) => { res.send('Hello Word!');});
 
+require('./routes')(app);
+
 app.listen(port, () => {
     console.log(`Aplicação rodando na porta ${port}`);
 });
