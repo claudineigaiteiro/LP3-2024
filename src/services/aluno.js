4927c20f9adfadce7a6f3f12edde9125f438a4a0
claudinei <claudineigaiteiro@gmail.com>
2024-02-22 21:05:00 -0300

    Criação service aluno

---
+++ src/services/aluno.js
@@ -0,0 +1,16 @@
+const getAluno = async (params) => {
+    let aluno = {}
+    if (params.name === "Claudinei"){
+        aluno.id = 16105
+        aluno.nome = "Claudinei Roberto"
+        aluno.sobrenome = "Mayolo"
+        aluno.notas = []
+        aluno.notas.push(10)
+        aluno.notas.push(9)
+    } else {
+        throw "Aluno não cadastrado!"
+    }
+    return aluno
+}
+
+module.exports.getAluno = getAluno
