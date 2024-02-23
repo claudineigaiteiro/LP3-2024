dc4742913f4c14816e394c87637cf96d828efc96
claudinei <claudineigaiteiro@gmail.com>
2024-02-22 21:56:11 -0300

    Ajuste

--- src/controllers/alunos.js
+++ src/controllers/alunos.js
@@ -2,7 +2,7 @@ const alunoService = require('../services/alunos')
 
 const getAlunos = async (req, res, next) => {
     try {
-        const retorno = await alunoService.getAlunos(req.params)
+        const retorno = await alunoService.getAluno(req.params)
         res.status(201).json(retorno)
     } catch (err) {
         res.status(500).send(err)
