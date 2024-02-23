bdb5341c57efc6eae02e50ba6a8d3a5fc2b402f7
claudinei <claudineigaiteiro@gmail.com>
2024-02-22 22:59:58 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => { res.send('Hello Word! Fune');});
+app.get('/', (req, res) => { res.send('Hello Word!');});
 
 require('./routes')(app);
 
